a7372f8d2cc15c4ba57d832f446b82380378b350
pepefrog228228 <pepefrog228228228@gmail.com>
2024-10-23 20:39:14 +0500

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-the_number_of_completed_homework = 12
-number_of_hours_spent = 1.5
-course_name = 'Python'
-time_for_one_task = 0.125
-print(number_of_hours_spent / 12)
-print('курс', course_name,',' , 'всего задач:', the_number_of_completed_homework,',', 'затрачено часов:',number_of_hours_spent,',', 'среднее время выполнения:', time_for_one_task)
